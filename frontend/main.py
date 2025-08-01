4c521d122dad697f747f997837b7f3ae518fec28
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-08-01 16:57:35 +0800

    Create main.py

---
+++ frontend/main.py
@@ -0,0 +1,22 @@
+from nicegui import ui, app
+
+import settings
+from ui.pages import register_pages
+
+# 注册 pages
+register_pages()
+
+# 挂载静态资源
+app.add_static_files("/static", str(settings.SOURCE_DIR / "static"))
+
+if __name__ == '__main__':
+    ui.run(title=settings.TITLE,
+           favicon=settings.FAVICON,
+           host=settings.HOST,
+           port=settings.PORT,
+           reconnect_timeout=settings.RECONNECT_TIMEOUT,
+           native=settings.NATIVE,
+           reload=False,
+           show=False,
+           on_air=settings.ON_AIR_TOKEN,
+           storage_secret=settings.STORAGE_SECRET)
